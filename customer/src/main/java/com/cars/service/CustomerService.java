e61faafff3ce8541072ce41b0d040ebd8d8cfb99
alexMatsievsky <matsievsky@gmail.com>
2023-03-09 22:14:38 +0200

    psql

--- customer/src/main/java/com/cars/service/CustomerService.java
+++ customer/src/main/java/com/cars/service/CustomerService.java
@@ -1,4 +1,7 @@
 package com.cars.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CustomerService {
 }
---
+++ customer/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS customer (
+    id UUID                 NOT NULL PRIMARY KEY,
+    email VARCHAR(200)      NOT NULL UNION,
+    firstName VARCHAR(100)   NOT NULL,
+    lastName VARCHAR(100)   NOT NULL
+);
+--CREATE DATABASE IF NOT EXISTS dealershipDB;
